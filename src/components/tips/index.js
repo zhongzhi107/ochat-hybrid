b8283d1fa3b29006b43b9b2f973d2e18d15e6711
zhi.zhong <zhi.zhong@163.com>
2015-10-19 10:07:12 +0800

    Add readonly decoration

--- src/components/tips/index.js
+++ src/components/tips/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 import React, {Component, PropTypes} from 'react';
+import {readonly} from 'core-decorators';
 import './tips.less';
 
 export default class Tips extends Component {
 
+  @readonly
   static propTypes = {
     children: PropTypes.string.isRequired
   }
